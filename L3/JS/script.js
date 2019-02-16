b74e992b0810c5751334fd7cb18be6e5f13cefb0
Student <you@example.com>
2019-02-16 13:48:31 +0200

    First commit

---
+++ L3/JS/script.js
@@ -0,0 +1,27 @@
+var array = ["Пушок","Барсик","Рыжик"];
+array.pop();//убирает последний елемент
+console.log(array);
+array.push("Кит","Слон","Яблоко",77,true,["String","String2"]);//добавляет последователный элемент
+console.log(array);
+
+
+
+
+
+
+
+
+
+
+
+// for (i = 0;i < array.length; i++ ){
+// 	if (array[i] == "Барсик"){
+// 		console.log(`У меня есть ${array[i]} Мурка `);
+// 	}
+// 	if (array[i] == "Рыжик"){
+
+// 	}
+// 	if (array[i] == "Пушок"){
+// 		console.log(`У меня есть ${array[i]}`); 
+// 	}
+// }
